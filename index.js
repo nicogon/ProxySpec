141d4d8380064c82dc7695bef7950ae245e883ba
nicogon <nicogon@gmail.com>
2021-04-20 12:25:39 -0300

    use coco api

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const app = express();
 const port = 5000;
 
 
+/*
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+*/
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
@@ -19,6 +22,9 @@ app.listen(port, () => {
 app.post('/proxies', handleCreateProxy);
 app.get('/proxies/:id', handleGetProxy);
 app.get('/', handleMain);
+
+app.get('/uno/dos', handleMain);
+
 
 function handleMain(req, res) {
   res.send(200, "main");
@@ -45,7 +51,6 @@ function handleCreateProxy(req, res) {
     proxyId
   });
 
-
   res.redirect('/');
 }
 
@@ -64,15 +69,27 @@ function createProxy({
 
   const apiProxy = httpProxy.createProxyServer({});
 
-  app.get(`/proxies/${proxyId}/proxy/*`, function (req, res) {
-    //req.url = req.url.replace(`/proxies/${proxyId}/proxy`, "");
-    console.log("redirect",req.url, "olis", target)
+  app.get(`/proxies/${proxyId}/proxy/**`, function (req, res) {
+    req.url = req.url.replace(`/proxies/${proxyId}/proxy`, "");
+    console.log("redirect", req.url, "olis", target)
     return apiProxy.web(req, res, {
-      target
+      target,
+      changeOrigin: true
     });
   });
 
-  apiProxy.on("error", (e)=>console.log(e))
+
+
+
+  apiProxy.on('proxyReq', function (proxyReq, req) {
+    // keep a ref
+    req._proxyReq = proxyReq;
+  });
+
+  apiProxy.on('error', function (err, req, res) {
+    console.log(err)
+    res.send(500)
+  });
 
   apiProxy.on("proxyRes", function (proxyRes, req, res) {
     console.log("proxypass", req.url, proxyRes.statusCode);
@@ -87,4 +104,7 @@ function createProxy({
   });
 }
 
-createProxy({target:'http://all-around.herokuapp.com/all', proxyId:1})
+createProxy({
+  target: 'http://all-around.herokuapp.com/all',
+  proxyId: 1
+})
